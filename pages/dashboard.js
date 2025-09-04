d333662757f26f694ca7c445df01d402a17a11c9
SAKI-ROMINA <rominamazzeo@gmail.com>
2025-09-04 17:49:27 -0300

    Create dashboard.js

---
+++ pages/dashboard.js
@@ -0,0 +1,9 @@
+// pages/dashboard.js
+export default function Dashboard() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'Arial' }}>
+      <h1>Bienvenido al Portal SAKI 🚀</h1>
+      <p>Has iniciado sesión correctamente.</p>
+    </div>
+  )
+}
