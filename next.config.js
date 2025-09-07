be38ffe0f2ce410febb83222c91074393d97fddb
SAKI-ROMINA <rominamazzeo@gmail.com>
2025-09-06 23:55:26 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true, // no frena el build si hay errores de TS
-  },
-  eslint: {
-    ignoreDuringBuilds: true, // no frena por ESLint
-  },
-};
-
-module.exports = nextConfig;
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true }
+}
+module.exports = nextConfig
