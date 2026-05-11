1fe30166789cfeae67a7e4aae144bbe69f947a41
SAKI-ROMINA <rominamazzeo@gmail.com>
2026-05-11 09:48:48 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,12 @@
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
-import { supabase } from "../lib/supabaseClient";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
 export default function Home() {
   const router = useRouter();
 
   useEffect(() => {
-    const checkSession = async () => {
-      try {
-        const { data, error } = await supabase.auth.getSession();
-
-        if (error) {
-          console.error('Error al obtener la sesión:', error.message);
-          router.replace('/login');
-          return;
-        }
-
-        if (data?.session) {
-          router.replace('/dashboard'); // si está logueado
-        } else {
-          router.replace('/login'); // si no está logueado
-        }
-      } catch (err) {
-        console.error('Error inesperado:', err);
-        router.replace('/login');
-      }
-    };
-
-    checkSession();
+    router.replace("/dia");
   }, [router]);
 
-  return <p>Cargando...</p>;
+  return <p>Cargando Portal Día...</p>;
 }
